e2ee207e65b35203ade2dbd44d9c933943e34531
kocaserife <105380391+kocaserife@users.noreply.github.com>
2023-10-21 23:49:06 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import { topla, anan, array } from "./my-module.js";
+
+console.log(topla(5, 8));
+
+console.log(anan);
+
+console.log(array);
+
+import FetchData from "./array-fnc.js";
+
+(async () => {
+  console.log(await FetchData(1));
+})();
